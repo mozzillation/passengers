73b0e2434dc08c0a29b8328c62bf135c327360ae
Giuliano Mozzillo <mozzillation@me.com>
2018-12-05 03:30:22 +0100

    5 dic

--- website/assets/js/bundle.js
+++ website/assets/js/bundle.js
@@ -16,19 +16,91 @@ var preload = $("#preload"),
 
 
 
-
 function loadData() {
 
+    $.getJSON("./data/main.json", function (json) {
 
 
-    $('.place').hover(function () {
-        var $this = $(this);
-        var cord = $this.data("cord");
-        map.flyTo({
-            zoom: 15,
-            center: cord
+        json.places.forEach(function (marker) {
+
+            // MAKERS
+            var el = document.createElement("div");
+            $(el).addClass("marker").attr("data-place", marker.properties.id).attr("data-cord", '[' + marker.geometry.coordinates + ']');
+
+
+            // SIDEBAR
+            var element = document.createElement("article");
+            $(element).addClass("place").attr('data-place', marker.properties.id).attr("data-cord", '[' + marker.geometry.coordinates + ']').html("  <div class='place_thumb'></div><div class='place_address'>" + marker.properties.description + "</div><div class = 'place_time' >" + marker.properties.timestamp + "</div>")
+            $('#sidebar .places').append(element)
+
+
+
+            new mapboxgl.Marker(el)
+                .setLngLat(marker.geometry.coordinates)
+                .addTo(map);
         });
+
+
+        $('.place, .marker').hover(function () {
+            var id = $(this).data("place");
+            $('[data-place="' + id + '"]').toggleClass('highlight')
+        })
+
+        $('.place, .marker').click(function () {
+
+
+
+
+            $('.passenger').remove();
+
+            var $this = $(this);
+            var cord = $this.data("cord");
+            var id = $this.data("place");
+
+            $('[data-place="' + id + '"]').toggleClass('is-active');
+
+            var lat = cord[0];
+            var long = cord[1];
+
+
+            console.log(id);
+
+            map.flyTo({
+                zoom: 15,
+                center: cord
+            });
+
+
+            $.getJSON('./data/places/gustavoModena.json', function (json) {
+
+
+                json.passengers.forEach(function (marker) {
+
+
+                    var element = document.createElement("li");
+
+                    $(element).html(marker.mac);
+                    $('#subsidebar').append(element);
+
+                    var el = document.createElement("div");
+                    var randCoord = getRandomLocation(lat, long, 250);
+                    el.className = "passenger";
+
+                    new mapboxgl.Marker(el)
+                        .setLngLat([randCoord.latitude, randCoord.longitude])
+                        .addTo(map);
+
+                    $('#subsidebar').addClass('is-active');
+
+
+
+                })
+
+            })
+        })
+
     })
+
 
 }
 
@@ -77,15 +149,9 @@ function initMap() {
 
 
 
+
 $(document).ready(function () {
     //    animateLogo();    
-
-
-    $.getJSON("./data/main.json", function (json) {
-
-        console.log(json);
-
-    })
 
     initMap();
 });
